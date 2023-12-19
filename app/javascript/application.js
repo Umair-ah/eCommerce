65bebbb93aa0dfcbff2f4c6a7976338e0da1a2e7
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-12-19 19:48:40 +0530

    installed flowbite and rails app initialized

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "flowbite/dist/flowbite.turbo.js";
+import "flowbite-datepicker";
+import "flowbite/dist/datepicker.turbo.js";
