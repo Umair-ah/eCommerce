f25253c45875dd06ec4558bc6bbf9ce516155477
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-12-20 09:54:29 +0530

    add_to_cart and destroy_from_cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,29 @@
 class LineItemsController < ApplicationController
 
   def buy_now
-    # check if the product is already present
+    check_and_add_to_cart
+    redirect_to carts_path(@current_cart)
+  end
+
+  def add_to_cart
+    check_and_add_to_cart
+    if @line_item.quantity == 1
+      @line_item.quantity += (params[:quantity].to_i - 1)
+    else
+      @line_item.quantity += params[:quantity].to_i
+    end
+    @line_item.save
+    redirect_to product_path(@selected_product)
+  end
+
+  def destroy_from_cart
+    @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.destroy
+    redirect_to carts_path(@current_cart)
+  end
+
+  def check_and_add_to_cart
     @selected_product = Product.find(params[:product_id])
     if @current_cart.products.include?(@selected_product)
       @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
@@ -11,11 +33,6 @@ class LineItemsController < ApplicationController
       @line_item.product = @selected_product
       @line_item.save
     end
-    redirect_to carts_path(@current_cart)
   end
-
-  def add_to_cart
-  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post "line_items_buy_now/:product_id", to: "line_items#buy_now", as: "line_item_buy_now"
   post "line_items_add_to_cart/:product_id", to: "line_items#add_to_cart", as: "line_item_add_to_cart"
 
+  delete "line_item_delete_from_cart/:product_id", to: "line_items#destroy_from_cart", as: "destroy_from_cart"
 end
