61f7973c6fac6ed69a5c26b36aa50699ca7799ed
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-12-22 15:00:07 +0530

    carts turbo_frames

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -20,7 +20,49 @@ class LineItemsController < ApplicationController
     @selected_product = Product.find(params[:product_id])
     @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
     @line_item.destroy
-    redirect_to carts_path(@current_cart)
+    #redirect_to carts_path(@current_cart)
+    respond_to do |format|
+      format.turbo_stream{
+        render turbo_stream:
+        [
+          turbo_stream.remove(
+            "line_item_#{@line_item.id}"
+          ),
+          turbo_stream.update(
+            "Cart_#{@current_cart.id}",
+            partial: "carts/sub_total",
+            locals: {cart: @current_cart}
+          ),
+          turbo_stream.update(
+            "cart_icon_#{@current_cart.id}",
+            partial: "layouts/cart_icon_size",
+            locals: {cart: @current_cart}
+          ),
+        ]
+      }
+    end
+  end
+
+  def add_quantity
+    @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.quantity += 1
+    @line_item.save
+    #redirect_to carts_path(@current_cart)
+    turbo_frame_tag_respond_for_quantity
+    
+  end
+
+  def subtract_quantity
+    @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.quantity -= 1
+    @line_item.save
+    if @line_item.quantity == 0
+      @line_item.destroy
+    end
+    #redirect_to carts_path(@current_cart)
+    turbo_frame_tag_respond_for_quantity
   end
 
   def check_and_add_to_cart
@@ -32,6 +74,27 @@ class LineItemsController < ApplicationController
       @line_item.cart = @current_cart
       @line_item.product = @selected_product
       @line_item.save
+    end
+  end
+
+  def turbo_frame_tag_respond_for_quantity
+    respond_to do |format|
+      format.turbo_stream{
+        render turbo_stream:
+        [
+          turbo_stream.update(
+            "quantity_#{@line_item.id}",
+            partial: "carts/quantity",
+            locals: {line_item: @line_item}
+          ),
+
+          turbo_stream.update(
+            "Cart_#{@current_cart.id}",
+            partial: "carts/sub_total",
+            locals: {cart: @current_cart}
+          )
+        ]
+      }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   
   post "line_items_buy_now/:product_id", to: "line_items#buy_now", as: "line_item_buy_now"
   post "line_items_add_to_cart/:product_id", to: "line_items#add_to_cart", as: "line_item_add_to_cart"
+  post "line_items_add_quantity/:product_id", to: "line_items#add_quantity", as: "line_item_add_quantity"
+  post "line_items_subtract_quantity/:product_id", to: "line_items#subtract_quantity", as: "line_item_subtract_quantity"
+
+
+
 
   delete "line_item_delete_from_cart/:product_id", to: "line_items#destroy_from_cart", as: "destroy_from_cart"
 end
