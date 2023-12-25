6f48e374e4127678de2940a2e5f89f2fb7870f2d
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-12-25 23:46:54 +0530

    flash messages and some errors fixed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,13 +4,21 @@ class LineItemsController < ApplicationController
     check_and_add_to_cart
     @line_item.quantity += params[:quantity].to_i
     @line_item.save
+    flash.now[:notice] = "#{params[:quantity]} items added to cart"
     respond_to do |format|
       format.turbo_stream{
         render turbo_stream:
-        turbo_stream.update(
-          "product_#{@selected_product.id}_user_#{current_user.id}",
-          partial: "form_for_add_to_cart",
-        )
+        [
+          turbo_stream.update(
+            "product_#{@selected_product.id}_user_#{current_user.id}",
+            partial: "form_for_add_to_cart",
+          ),
+
+          turbo_stream.update(
+            "flash_#{current_user&.id}",
+            partial: "layouts/flash",
+          )
+        ]
       }
     end
   end
@@ -32,13 +40,31 @@ class LineItemsController < ApplicationController
       @line_item.quantity += params[:quantity].to_i
     end
     @line_item.save
-    redirect_to product_path(@selected_product)
+    flash.now[:notice] = "#{params[:quantity]} items added to cart"
+    respond_to do |format|
+      format.turbo_stream{
+        render turbo_stream:
+        [
+          turbo_stream.update(
+            "add_to_cart_#{current_user&.id}",
+            partial: "products/add_to_cart_form",
+            locals: {product: @selected_product}
+          ),
+
+          turbo_stream.update(
+            "flash_#{current_user&.id}",
+            partial: "layouts/flash",
+          )
+        ]
+      }
+    end
   end
 
   def destroy_from_cart
     @selected_product = Product.find(params[:product_id])
     @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
     @line_item.destroy
+    flash.now[:alert] = "Item removed"
     #redirect_to carts_path(@current_cart)
     respond_to do |format|
       format.turbo_stream{
@@ -62,6 +88,11 @@ class LineItemsController < ApplicationController
             "checkout_btn_#{current_user.id}",
             partial: "carts/razorpay",
             locals: {cart: @current_cart}
+          ),
+
+          turbo_stream.update(
+            "flash_#{current_user&.id}",
+            partial: "layouts/flash",
           )
         ]
       }
@@ -94,11 +125,13 @@ class LineItemsController < ApplicationController
     @selected_product = Product.find(params[:product_id])
     if @current_cart.products.include?(@selected_product)
       @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+      flash.now[:alert] = "Item is already in cart, add quantity"
     else
       @line_item = LineItem.new
       @line_item.cart = @current_cart
       @line_item.product = @selected_product
       @line_item.save
+      flash.now[:notice] = "Item added to cart!"
     end
   end
 
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import FlashController from "./flash_controller"
+application.register("flash", FlashController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
